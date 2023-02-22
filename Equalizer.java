036b1de415468602bc22e640f98a3fc3c0fa8508
bbarbis <baratebarbara2020@gmail.com>
2023-02-22 15:58:54 +0100

    toroltem a kommenteket

--- Equalizer.java
+++ Equalizer.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package pkg0222oraimunka;
 
-/**
- *
- * @author JMSZ-User
- */
+
 public class Equalizer {
-    
+    public static void main(String[] args) {
+        
+    }
 }
