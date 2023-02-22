d0c26b7440db393c909c3352c93ef3f6b2170329
bbarbis <baratebarbara2020@gmail.com>
2023-02-22 15:07:05 +0100

    kiirat

--- Main.java
+++ Main.java
@@ -5,6 +5,30 @@ public class Main {
 
     
     public static void main(String[] args) {
+        int osszeg = elso10SzamOsszege();
+        String kimenet = "Az első 10 szám összege: " + osszeg + "\n";
+        kiir(kimenet);
+        
+        int szam1 = 7, szam2 = 4;
+        osszeg = osszead(szam1, szam2);
+        String s = String.format("%d + %d = %d\n", szam1, szam2, osszeg);
+        kiir(s);
+        
+    }
+
+    private static void elso10SzamOsszege() {
+        int osszeg = 0;
+        for(int i=0; i<10; i++){
+            osszeg += i;
+        }
+        return;
+    }
+
+    private static void osszead(int a, int b) {
+        return a+b;
+    }
+
+    private static void kiir(String szoveg) {
         System.out.print(szoveg);
     }
     
