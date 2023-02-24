458c7d8adbaafc0edbfc5faf8e82c7424cc249da
bbarbis <baratebarbara2020@gmail.com>
2023-02-24 12:01:17 +0100

    parameterezes

--- Equalizer.java
+++ Equalizer.java
@@ -1,8 +1,31 @@
 package pkg0222oraimunka;
 
-
-public class Equalizer {
-    public static void main(String[] args) {
+import java.util.Random;
         
+public class Equalizer {
+    
+    private static void Random Random = new Random();
+    
+    public static void main(String[] args) {
+        equalizer_5_sorban();
+        
+        
+    }
+
+    private static void equalizer_5_sorban() {
+        eq();
+        eq();
+        eq();
+        eq();
+        eq();
+    }
+
+    private static void eq() {
+        int db = rnd.nextInt(3, 8);
+        String szin =  "\u001B[45m";
+        for (int i = 0; i< db; i++){
+            System.out.print(szin + " ");
+        }
+        System.out.println("");
     }
 }
--- Main.java
+++ Main.java
@@ -5,41 +5,40 @@ public class Main {
 
     
     public static void main(String[] args) {
-        Elso10Szamkiirrva();
-        KettoSzamKiirva();
-        NegyszamOsszegKiirva();
-        gyokKivonas();
-    }
-
-    private static void gyokKivonas() {
-        int a = 3, b=4, c=2;
-        osszeg =osszead(osszeg, c);
+        
+        
+        int a = 3, b = 4, c = 2;
+        osszeg = osszead(osszeg, c);
         s = "%d+%d+%d gyöke:".formatted(a,b,c);
         double gyok = Math.sqrt(osszeg);
         kimenet = String.format("%s %.4f\n", s, gyok);
         kiir(kimenet);
-    }
-
-    private static void NegyszamOsszegKiirva() {
+        
         osszeg = osszead(1, 2);
         osszeg = osszead(osszeg, 3);
         osszeg = osszead(osszeg, 4);
-        kiir("1+2+3+4="+osszeg+"\n");
-    }
-
-    private static void KettoSzamKiirva() {
+        kiir("1+2+3+4=" + osszeg + "\n");
+        
         int szam1 = 7, szam2 = 4;
         osszeg = osszead(szam1, szam2);
         String s = String.format("%d + %d = %d\n", szam1, szam2, osszeg);
         kiir(s);
-    }
-
-    private static void Elso10Szamkiirrva() {
+        
         int osszeg = elso10SzamOsszege();
         String kimenet = "Az első 10 szám összege: " + osszeg + "\n";
         kiir(kimenet);
+        
     }
 
+    
+
+   
+        
+    }
+
+    
+
+    
     private static int elso10SzamOsszege() {
         int osszeg = 0;
         for(int i=0; i<10; i++){
