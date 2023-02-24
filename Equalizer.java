386b4ad13feca072d0543983a97d9071f468631b
bbarbis <baratebarbara2020@gmail.com>
2023-02-24 12:10:30 +0100

    equalizer

--- Equalizer.java
+++ Equalizer.java
@@ -1,18 +1,15 @@
 package pkg0222oraimunka;
 
 import java.util.Random;
-        
+
+
 public class Equalizer {
-    
-    private static void Random Random = new Random();
-    
+    private static Random rnd = new Random();
     public static void main(String[] args) {
-        equalizer_5_sorban();
-        
-        
+        equalizer_5sorban();
     }
 
-    private static void equalizer_5_sorban() {
+    private static void equalizer_5sorban() {
         eq();
         eq();
         eq();
