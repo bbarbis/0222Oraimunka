f340881413f3a336961c50d875577668a3dd1864
bbarbis <baratebarbara2020@gmail.com>
2023-02-24 12:16:18 +0100

    tulterheles

--- Equalizer.java
+++ Equalizer.java
@@ -6,21 +6,28 @@ import java.util.Random;
 public class Equalizer {
     private static Random rnd = new Random();
     public static void main(String[] args) {
-        equalizer_5sorban();
+        equalizer_5_sorban();
+        
     }
 
-    private static void equalizer_5sorban() {
+    private static void equalizer_5_sorban() {
         eq();
+        eq(12);
         eq();
-        eq();
-        eq();
+        eq(8);
         eq();
     }
 
     private static void eq() {
         int db = rnd.nextInt(3, 8);
+        eq(db);
+    }
+
+    
+
+    private static void eq(int hossz) {
         String szin =  "\u001B[45m";
-        for (int i = 0; i< db; i++){
+        for (int i = 0; i< hossz; i++){
             System.out.print(szin + " ");
         }
         System.out.println("");
